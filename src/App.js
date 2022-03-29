897cb7981c3e50ac5ee4299682d77b47ec2f7ece
Robert Lopez <robertlopez@Roberts-MBP.lan>
2022-03-28 20:29:08 -0700

    added front end forms

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/Header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Router> */}
+        <div className="container">
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      {/* </Router> */}
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+
+function Header(){
+    return(
+        <header className="header">
+        <div>
+            <Link to='/'>Projects</Link>
+        </div>
+        <ul>
+            <li>
+            <Link to='/login'>Login</Link>
+            </li>
+            <li>
+            <Link to='/register'>Register</Link>
+            </li>
+        </ul>
+        </header>
+    )
+}
+export default Header
